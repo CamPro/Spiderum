f74cf35bacdd554b547113a8ad4a73dac4328e34
campx <campxpro@gmail.com>
2024-01-20 00:47:00 +0700

    add

---
+++ Spiderum/Spiderum.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Spiderum
+{
+    partial class Spiderum
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Spiderum));
+            this.buttonSpiderum = new System.Windows.Forms.Button();
+            this.buttonCrawlUser = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.comboSpiderum = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonSpiderum
+            // 
+            this.buttonSpiderum.Location = new System.Drawing.Point(12, 62);
+            this.buttonSpiderum.Name = "buttonSpiderum";
+            this.buttonSpiderum.Size = new System.Drawing.Size(213, 62);
+            this.buttonSpiderum.TabIndex = 2;
+            this.buttonSpiderum.Text = "Crawl Post";
+            this.buttonSpiderum.UseVisualStyleBackColor = true;
+            this.buttonSpiderum.Click += new System.EventHandler(this.buttonSpiderum_Click);
+            // 
+            // buttonCrawlUser
+            // 
+            this.buttonCrawlUser.Location = new System.Drawing.Point(259, 62);
+            this.buttonCrawlUser.Name = "buttonCrawlUser";
+            this.buttonCrawlUser.Size = new System.Drawing.Size(213, 62);
+            this.buttonCrawlUser.TabIndex = 3;
+            this.buttonCrawlUser.Text = "Crawl User";
+            this.buttonCrawlUser.UseVisualStyleBackColor = true;
+            this.buttonCrawlUser.Click += new System.EventHandler(this.buttonCrawlUser_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(12, 12);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 35);
+            this.buttonStop.TabIndex = 0;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(239, 22);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(87, 15);
+            this.labelStatus.TabIndex = 5;
+            this.labelStatus.Text = "Are you ready?";
+            // 
+            // comboSpiderum
+            // 
+            this.comboSpiderum.FormattingEnabled = true;
+            this.comboSpiderum.Items.AddRange(new object[] {
+            "new",
+            "hot",
+            "top",
+            "controversial",
+            "follow"});
+            this.comboSpiderum.Location = new System.Drawing.Point(93, 19);
+            this.comboSpiderum.Name = "comboSpiderum";
+            this.comboSpiderum.Size = new System.Drawing.Size(132, 23);
+            this.comboSpiderum.TabIndex = 1;
+            // 
+            // Spiderum
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.comboSpiderum);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonCrawlUser);
+            this.Controls.Add(this.buttonSpiderum);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Spiderum";
+            this.Text = "Spiderum";
+            this.Load += new System.EventHandler(this.Spiderum_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonSpiderum;
+        private System.Windows.Forms.Button buttonCrawlUser;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.ComboBox comboSpiderum;
+    }
+}
+
---
+++ Spiderum/Spiderum.cs
@@ -0,0 +1,261 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spiderum
+{
+    public partial class Spiderum : Form
+    {
+        string status = string.Empty;
+
+        List<string> users = new List<string>();
+        List<string> emails = new List<string>();
+
+        private delegate void DelegateUpdate();
+
+        bool isRunning = false;
+
+        string header = "_id \t name \t email \t created_at \t post_score \t salt \t hashed_password \n";
+
+        public Spiderum()
+        {
+            InitializeComponent();
+        }
+
+        private void Spiderum_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("emails.txt"))
+            {
+                emails = File.ReadAllLines("emails.txt").ToList();
+            }
+            if (File.Exists("users.txt"))
+            {
+                users = File.ReadAllLines("users.txt").ToList();
+            }
+        }
+
+        private void UpdateStatus(string status)
+        {
+            this.status = status;
+            UpdateStatus();
+        }
+
+        public void UpdateStatus()
+        {
+            if (labelStatus.InvokeRequired)
+            {
+                DelegateUpdate d = new DelegateUpdate(UpdateStatus);
+                this.Invoke(d);
+            }
+            else
+            {
+                labelStatus.Text = status;
+            }
+        }
+
+        private void UpdateStatus2(string status)
+        {
+            this.status = status;
+            UpdateStatus2();
+        }
+        public void UpdateStatus2()
+        {
+            if (buttonCrawlUser.InvokeRequired)
+            {
+                DelegateUpdate d = new DelegateUpdate(UpdateStatus2);
+                this.Invoke(d);
+            }
+            else
+            {
+                buttonCrawlUser.Text = status;
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            isRunning = false;
+        }
+
+        private void Crawl(string name)
+        {
+            for (int page = 1; page < 100; page++)
+            {
+                try
+                {
+                    UpdateStatus(name + " : " + page);
+
+                    string text = new WebClient().DownloadString("https://spiderum.com/api/v2/user/getUserComments?profile_name=" + name + "&page=" + page);
+
+                    JObject obj = JObject.Parse(text);
+
+                    if (obj["userComments"].Count() == 0)
+                    {
+                        break;
+                    }
+
+                    string data = "";
+
+                    foreach (var item in obj["userComments"])
+                    {
+                        string email = string.Empty;
+
+                        if (item["user_id"].ToString().Contains("email"))
+                        {
+                            email = item["user_id"]["email"].ToString();
+                            if (!emails.Contains(email))
+                            {
+                                data += item["user_id"]["_id"].ToString() + "\t";
+                                data += item["user_id"]["name"].ToString() + "\t";
+                                data += item["user_id"]["email"].ToString() + "\t";
+                                data += item["user_id"]["created_at"].ToString() + "\t";
+                                data += item["user_id"]["post_score"].ToString() + "\t";
+                                data += item["user_id"]["salt"].ToString() + "\t";
+                                data += item["user_id"]["hashed_password"].ToString() + "\t";
+                                data += Environment.NewLine;
+                                emails.Add(email);
+                            }
+                        }
+                        email = item["post_id"]["creator_id"]["email"].ToString();
+                        if (!emails.Contains(email))
+                        {
+                            data += item["post_id"]["creator_id"]["_id"].ToString() + "\t";
+                            data += item["post_id"]["creator_id"]["name"].ToString() + "\t";
+                            data += item["post_id"]["creator_id"]["email"].ToString() + "\t";
+                            data += item["post_id"]["creator_id"]["created_at"].ToString() + "\t";
+                            data += item["post_id"]["creator_id"]["post_score"].ToString() + "\t";
+                            data += item["post_id"]["creator_id"]["salt"].ToString() + "\t";
+                            data += item["post_id"]["creator_id"]["hashed_password"].ToString() + "\t";
+                            data += Environment.NewLine;
+                            emails.Add(email);
+                        }
+
+                        if (!item.ToString().Contains("parent_id"))
+                        {
+                            continue;
+                        }
+                        if (item["parent_id"].ToString() == "")
+                        {
+                            continue;
+                        }
+                        if (!item["parent_id"]["user_id"].ToString().Contains("email"))
+                        {
+                            continue;
+                        }
+
+                        email = item["parent_id"]["user_id"]["email"].ToString();
+                        if (!emails.Contains(email))
+                        {
+                            data += item["parent_id"]["user_id"]["_id"].ToString() + "\t";
+                            data += item["parent_id"]["user_id"]["name"].ToString() + "\t";
+                            data += item["parent_id"]["user_id"]["email"].ToString() + "\t";
+                            data += item["parent_id"]["user_id"]["created_at"].ToString() + "\t";
+                            data += item["parent_id"]["user_id"]["post_score"].ToString() + "\t";
+                            data += item["parent_id"]["user_id"]["salt"].ToString() + "\t";
+                            data += item["parent_id"]["user_id"]["hashed_password"].ToString() + "\t";
+                            data += Environment.NewLine;
+                            emails.Add(email);
+                        }
+                    }
+
+                    File.AppendAllText("data.txt", data);
+                }
+                catch (Exception ex)
+                {
+                    UpdateStatus(ex.Message);
+                    File.AppendAllText("errors.txt", name + " : " + ex.Message + Environment.NewLine);
+                    break;
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        private void buttonSpiderum_Click(object sender, EventArgs e)
+        {
+            UpdateStatus("Start...");
+            string typeFeed = comboSpiderum.Text;
+            int index = 1;
+            int totalItems = 9999;
+
+            isRunning = true; //https://spiderum.com/api/v2/user/getUserSavedPosts?page=1
+
+            Task task = new Task(() =>
+            {
+                for (int i = index; i < totalItems && isRunning; i++)
+                {
+                    string text = new WebClient().DownloadString("https://spiderum.com/api/v1/feed/getPostsInFeed?type=" + typeFeed + "&page=" + i);
+
+                    JObject array = JObject.Parse(text);
+
+                    totalItems = Convert.ToInt32(array["posts"]["totalItems"]) / 20;
+
+                    UpdateStatus(i + " / " + totalItems);
+
+                    foreach (var item in array["posts"]["items"])
+                    {
+                        string name = item["creator_id"]["name"].ToString();
+
+                        if (users.Contains(name))
+                        {
+                            continue;
+                        }
+
+                        UpdateStatus(i + " : " + name);
+                        users.Add(name);
+
+                        Crawl(name);
+
+                        File.WriteAllLines("emails.txt", emails);
+                        File.WriteAllLines("users.txt", users);
+                    }
+                    Thread.Sleep(100);
+                }
+
+                UpdateStatus("Done");
+            });
+            task.Start();
+        }
+
+
+        private void buttonCrawlUser_Click(object sender, EventArgs e)
+        {
+            UpdateStatus("Start...");
+            isRunning = true;
+
+            var lines = File.ReadAllLines("data.txt");
+
+            Task task = new Task(() =>
+            {
+                for (int i = 0; i < lines.Length && isRunning; i++)
+                {
+                    string name = lines[i].Split('\t')[1];
+
+                    if (users.Contains(name))
+                    {
+                        continue;
+                    }
+
+                    UpdateStatus2(i + "/" + lines.Length + " : " + name);
+                    users.Add(name);
+
+                    Crawl(name);
+
+                    File.WriteAllLines("emails.txt", emails);
+                    File.WriteAllLines("users.txt", users);
+                }
+                UpdateStatus("Done");
+            });
+            task.Start();
+        }
+
+    }
+}
